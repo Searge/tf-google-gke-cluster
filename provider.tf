2e839da61841bb44b3a6cb311f22a877dce8776c
Searge <ashikcygan@gmail.com>
2023-07-09 21:01:04 +0300

    :sparkles: (ft): Fill the gaps

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
+}
--- variables.tf
+++ variables.tf
@@ -1,13 +1,37 @@
 # Make terraform variables available to the module
-variable "GOOGLE_REGION" {
-  type        = string
-  description = "The region where the GKE cluster will be created."
-}
 variable "GOOGLE_PROJECT" {
   type        = string
   description = "The ID of the Google Cloud project where the GKE cluster will be created."
 }
+variable "GOOGLE_REGION" {
+  type        = string
+  description = "The region where the GKE cluster will be created."
+}
 variable "GKE_NUM_NODES" {
   type        = number
   description = "The number of nodes to create in the GKE cluster."
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "g1-small"
+  description = "Machine type"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = 2
+  description = "GKE nodes number"
+}
+
+variable "GKE_CLUSTER_NAME" {
+  type        = string
+  default     = "main"
+  description = "GKE cluster name"
+}
+
+variable "GKE_POOL_NAME" {
+  type        = string
+  default     = "main"
+  description = "GKE pool name"
 }
